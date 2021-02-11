be2efc9dca0ffa0c445a506d7ff89207361dbae4
Danny Bouwers <d.bouwers@gmail.com>
2021-02-12 00:29:56 +0100

    Tool parameters as variable Tweaked result overview - Display input parameters - Added runtime

--- nef2jpeg.sh
+++ nef2jpeg.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+#standard tool parameters
+#imagick=(-resize 1920x1920 -colorspace LAB -channel 0 -auto-level +channel -colorspace sRGB)
+imagick=(-auto-level -auto-gamma -normalize -resize 1920x1920)
+dcraw=(-q 3 -o 1 -6 -g 2.4 12.92 -w -T) # https://www.image-engineering.de/library/technotes/720-have-a-look-at-the-details-the-open-source-raw-converter-dcraw
+
+
 # parse params
 while [[ "$#" -gt 0 ]]; do
     case $1 in
@@ -21,6 +27,9 @@ logdirs=0
 logerrors=0
 logskipped=0
 logcreated=0
+
+#starttime
+starttime=`date +%s`
 
 IFS=$'\n'
 rawfiles=($(find "$photofolder" -type f -name "*NEF"))
@@ -60,12 +69,8 @@ for rawfile in "${rawfiles[@]}"
 	else
         tmpfile=$( echo -n "${jpegfile}" | openssl md5 )
         
-        # Convert RAW to TIFF
-        # https://www.image-engineering.de/library/technotes/720-have-a-look-at-the-details-the-open-source-raw-converter-dcraw
-        # https://www.mankier.com/1/dcraw
-        
-        #dcraw -q 3 -o 1 -6 -g 2.4 12.92 -w -T -c "${rawfile}" | convert - -resize 1920x1920 -auto-gamma -auto-level -normalize "${tmpfolder}/${tmpfile}.jpg"
-        dcraw -q 3 -o 1 -6 -g 2.4 12.92 -w -T -c "${rawfile}" | convert - -resize 1920x1920 -colorspace YCbCr -channel 0 -normalize +channel -colorspace sRGB "${tmpfolder}/${tmpfile}.jpg"
+        # Convert RAW to JPEG
+        dcraw ${dcraw[@]} -c "${rawfile}" | convert - ${imagick[@]} "${tmpfolder}/${tmpfile}.jpg"
         
         if [ $? -gt 0 ] ; then
             echo "Could not convert file ${rawfile}" >&2
@@ -94,14 +99,26 @@ for rawfile in "${rawfiles[@]}"
     ((logfiles+=1))
 done
 
-echo ""
-echo "-- SUMMARY --"
+#calculate runtime
+endtime=`date +%s`
+runtime=$((endtime-starttime))
+runhours=$((runtime / 3600));
+runminutes=$(( (runtime % 3600) / 60 ));
+runseconds=$(( (runtime % 3600) % 60 )); 
+
+#print statistics
+echo "-- SETTINGS --"
+echo "Scanned folder: ${photofolder}"
+echo "Output folders: ${jpegfolder}"
+echo "-- RESULTS --"
 echo "Processed RAW files: ${logfiles}"
 echo "Folders created: ${logdirs}"
 echo "Files created: ${logcreated}"
 echo "Files Skipped: ${logskipped}"
 echo "Errors: ${logerrors}"
+echo "Runtime: $runhours:$runminutes:$runseconds (hh:mm:ss)"
 
+#exit
 if [ $logerrors -gt 0 ] ; then
     exit 1
 else 
